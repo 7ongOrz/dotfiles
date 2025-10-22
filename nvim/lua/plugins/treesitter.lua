782a4a8c0f43403a493c36f8aaf422230113cd72
7ongOrz <32224938+7ongOrz@users.noreply.github.com>
2025-10-23 03:05:59 +0800

    fix(nvim): override nvim-treesitter main module to fix deprecated configs error
    Signed-off-by: 7ongOrz <32224938+7ongOrz@users.noreply.github.com>

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -4,6 +4,7 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   branch = "main", -- 使用 main 分支而不是已归档的 master 分支
+  main = "nvim-treesitter", -- 修复：使用新的主模块名（configs 已被废弃）
   opts = {
     ensure_installed = {
       "lua",
